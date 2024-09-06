442cc71064f3535694b2e3e70672700cae3281ee
Shubhamvip <shubhamlakhera616@gmail.com>
2024-09-06 09:53:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("6396884478:AAEJR8OBD3HxkNKKsdkAqcNBmOkFdCORDes")
 
   #Enter Your telegram username here without @
 OWNER_USERNAME=("venomXcrazy")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("5055265956")
 
 
 
